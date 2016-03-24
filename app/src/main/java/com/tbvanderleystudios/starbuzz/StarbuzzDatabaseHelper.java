c571642fe124f2110b043201c2a796edbf2559f7
TBVanderley4545 <TBVanderley@gmail.com>
2016-03-24 00:27:33 -0400

    | Added StarbuzzDatabaseHelper, which is an SQLiteOpenHelper. | | Created the DRINK table | | Created insertDrink method that eliminates duplicated code for adding a record to the table |

---
+++ app/src/main/java/com/tbvanderleystudios/starbuzz/StarbuzzDatabaseHelper.java
@@ -0,0 +1,46 @@
+package com.tbvanderleystudios.starbuzz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class StarbuzzDatabaseHelper extends SQLiteOpenHelper{
+    private static final String DB_NAME = "starbuzz"; // This is the database name
+    private static final int DB_VERSION = 1; // The version of the database
+
+    StarbuzzDatabaseHelper (Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // The code below used SQL to create an SQLite table called DRINK
+        db.execSQL("CREATE TABLE DRINK"
+                + "(_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "NAME TEXT"
+                + "DESCRIPTION TEXT"
+                + "IMAGE_RESOURCE_ID INTEGER);");
+
+        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+        insertDrink(db, "Cappucino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private static void insertDrink(SQLiteDatabase db,
+                                    String name,
+                                    String description,
+                                    int resourceID) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceID);
+
+        db.insert("DRINK", null, drinkValues);
+    }
+}
