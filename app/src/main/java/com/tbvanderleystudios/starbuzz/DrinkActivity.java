3550207d53ba9a5630e48d8586ff8e4f2ea9162b
tylervanderley <tbvanderley@gmail.com>
2016-03-26 20:18:07 -0400

    ** All work in this commit was focused on DrinkActivity.java **
    | Linked the SQLite Database to the UI in DrinkActivity by implementing an SQLiteOpenHelper, using the helper to create a StarbuzzDatabaseHelper object, and then creating a Cursor object from it |
    | Use the cursor to push information to the user interface in the if(cursor.moveToFirst()) condition |
    | Removed old code that pulled data from Drink class |

--- app/src/main/java/com/tbvanderleystudios/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/tbvanderleystudios/starbuzz/DrinkActivity.java
@@ -1,9 +1,14 @@
 package com.tbvanderleystudios.starbuzz;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -15,18 +20,50 @@ public class DrinkActivity extends AppCompatActivity {
         // Get the extra from the Intent
         int drinkNo = (Integer) getIntent().getExtras().get(DrinkCategoryActivity.EXTRA_DRINK_ID_TAG);
 
-        // Get the data from our drink array at the select index
-        Drink drink = Drink.drinks[drinkNo];
+        try {
+            // StarbuzzDatabaseHelper is created
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            // starbuzzDatabaseHelper creates a SQLiteDatabase object called db
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
 
-        // Establish connection between Java file and XML's views
-        ImageView drinkImageView = (ImageView) findViewById(R.id.drinkImageView);
-        TextView nameTextView = (TextView) findViewById(R.id.nameTextView);
-        TextView descriptionTextView = (TextView) findViewById(R.id.descriptionTextView);
+            // The Cursor is created by using the SQLiteDatabase query() method
+            Cursor cursor = db.query(
+                    "DRINK",
+                    new String[]{"NAME, DESCRIPTION, IMAGE_RESOURCE_ID"},
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkNo)},
+                    null,
+                    null,
+                    null
+            );
 
-        // set the views' information to the currently selected drink.
-        drinkImageView.setImageResource(drink.getImageResourceId());
-        drinkImageView.setContentDescription(drink.getName());
-        nameTextView.setText(drink.getName());
-        descriptionTextView.setText(drink.getDescription());
+            // Move to first entry found by cursor
+            if (cursor.moveToFirst()) {
+                // Get drink details from cursor
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int imageResourceId = cursor.getInt(2);
+
+                // Establish connection between Java file and XML's views
+                ImageView drinkImageView = (ImageView) findViewById(R.id.drinkImageView);
+                TextView nameTextView = (TextView) findViewById(R.id.nameTextView);
+                TextView descriptionTextView = (TextView) findViewById(R.id.descriptionTextView);
+
+                // Populate the UI with the data from the cursor
+                nameTextView.setText(nameText);
+                descriptionTextView.setText(descriptionText);
+                drinkImageView.setImageResource(imageResourceId);
+                drinkImageView.setContentDescription(descriptionText);
+
+            }
+
+            // Close the cursor and database
+            cursor.close();
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "The database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 }
